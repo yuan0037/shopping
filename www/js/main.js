3c7f264891693567d4cfa5ccb155f2218b008cb7
Bo Yuan <yuan0037@algonquinlive.com>
2014-12-08 17:58:43 -0500

    make more user friendly

--- www/js/main.js
+++ www/js/main.js
@@ -30,7 +30,7 @@ var app = {
 //        listeningElement.setAttribute('style', 'display:none;');
 //        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+       // console.log('Received Event: ' + id);
     }
 };
 
@@ -42,37 +42,38 @@ $(document).ready(function(){
 });
 
 function mySetup(){
-        console.log("device ready");
-        $("#shoppingList").append("<ol id=\"listItem\"></ol>");
+       //console.log("device ready");
+        $("#shoppingList").append("<ul id=\"listItem\"></ul>");
         $("#addToList").click(addItemToList);
         showListFromStorage();
 }
 //$(document).ready(function(){
 //        console.log("device ready");
-//        $("#shoppingList").append("<ol id=\"listItem\"></ol>");
+//        $("#shoppingList").append("<ul id=\"listItem\"></ul>");
 //        $("#addToList").click(addItemToList);
 //        showListFromStorage();
 //});
 
 function showListFromStorage(){    
     var tempResult= localStorage.getItem("grocery-yuan0037");
+    //console.log(tempResult);
     if (tempResult)
     {
         listOfItems = JSON.parse(tempResult);
         for (i=0; i<=listOfItems.length-1; i++)
         {
-            $("#listItem").append("<li positionID="+i+">"+"<p><img positionID=\""+i+"\" id=\"accept\" src=\"img/accept.png\"><span>"+listOfItems[i]+"</span><img positionID=\""+i+"\" id=\"delete\" src=\"img/delete.png\">"+"</p></li>");
+            $("#listItem").append("<li positionID="+i+">"+"<img positionID=\""+i+"\" id=\"delete\" src=\"img/delete.png\"><span>"+listOfItems[i]+"</span></li>");
         }
     }
     
-    $("img#accept").click(checkItem);
+    $("li").click(checkItem);
     $("img#delete").click(deleteItem);
 }
 
 function checkItem(ev){
-   console.log("check happened");
-    console.log("this="+$(this));
-    $(this).parent().toggleClass("selected");
+//   console.log("check happened");
+//    console.log("this="+$(this));
+    $(this).toggleClass("selected");
 }
 
 function deleteItem(ev){
@@ -97,11 +98,14 @@ function saveToStorage(){
 function addItemToList()
 {
     currentTotalCount=$("li").length;
-    $("ol").append("<li positionID="+currentTotalCount+">"+"<p><img positionID=\""+currentTotalCount+"\" id=\"accept\" src=\"img/accept.png\"><span>"+$("#itemName").val()+"</span><img positionID=\""+currentTotalCount+"\" id=\"delete\" src=\"img/delete.png\">"+"</p></li>");
+    
+    $("#listItem").append("<li positionID="+currentTotalCount+">"+"<img positionID=\""+currentTotalCount+"\" id=\"delete\" src=\"img/delete.png\"><span>"+$("#itemName").val()+"</span></li>");
+    
+  
     
     $("img[positionID="+currentTotalCount+"]").filter("#delete").click(deleteItem);
     $("img[positionID="+currentTotalCount+"]").filter("#accept").click(checkItem);
-
+   $("li[positionID="+currentTotalCount+"]").click(checkItem);
     saveToStorage();
 
     $("#itemName").val("");
